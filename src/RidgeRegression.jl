d9a63e7c2ce39555600fdea441765147354018f3
Eton Tackett <entackett@wisc.edu>
2026-04-07 11:01:58 -0500

    Remove RidgeRegression.jl changes from Design branch

--- src/RidgeRegression.jl
+++ src/RidgeRegression.jl
@@ -1,15 +1,5 @@
 module RidgeRegression
-<<<<<<< HEAD
-using CSV
-using DataFrames
-using LinearAlgebra
-
-include("bidiagonalization.jl")
-
-export compute_givens, rotate_rows!, rotate_cols!, bidiagonalize_with_H, apply_Ht_to_b
-=======
 
 # Write your package code here.
->>>>>>> dc1059095c388a2124e2afcece8cadac9d57831b
 
 end
