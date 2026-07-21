0a0c61cebf82b1c1094f22f3981369a1504a68e0
Eton Tackett <etont@icloud.com>
2026-07-21 12:38:18 -0400

    Export

--- src/RidgeRegression.jl
+++ src/RidgeRegression.jl
@@ -1,10 +1,11 @@
 module RidgeRegression
+
 using CSV
 using DataFrames
 using LinearAlgebra
 
 include("bidiagonalization.jl")
 
-export compute_givens, rotate_rows!, rotate_cols!, bidiagonalize_with_H, apply_Ht_to_b
+export compute_givens, rotate_rows!, rotate_cols!, bidiagonalize_with_H, apply_Ht_to_b, bidiagonalize_A
 
 end
